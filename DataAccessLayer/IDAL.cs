e07ac0a28642597da7e1d313c37426206c71dc6b
calime <adrien.calime@gmail.com>
2014-02-07 15:02:22 +0100

    ajout méthode interface IDal

--- DataAccessLayer/IDAL.cs
+++ DataAccessLayer/IDAL.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntitiesLayer;
 
 namespace DataAccessLayer
 {
     public interface IDAL
     {
+        IList<Joueur> GetAllJoueurs();
+        IList<Equipe> GetAllEquipes();
+        IList<Stade> GetAllStades();
+        IList<Match> GetAllMatchs();
+        IList<Coupe> GetAllCoupes();
+        IList<Utilisateur> GetAllUtilisateurs();
+        IList<Reservation> GetAllReservations();
 
+        void AjouterMatch(int inId, int inCoupeID, DateTime inDate, Equipe inDom, Equipe inVisiteur, double inPrix, int inSED, int inSEV, Stade inStade);
+        void SupprimerMatch(int inId);
+        void UpdateMatch(int inId, int inCoupeID, DateTime inDate, Equipe inDom, Equipe inVisiteur, double inPrix, int inSED, int inSEV, Stade inStade);
     }
 }
